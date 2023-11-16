d6d32213279f2e0a97658daaadea3717cf0b1250
wasaw <merenkovalexander@gmail.com>
2023-11-16 15:36:30 +0300

    Add calendar and balance form

--- Revenue/Presentation/View/HomeViewController.swift
+++ Revenue/Presentation/View/HomeViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 import SnapKit
 
 private enum Constants {
+    static let segmentContainerPaddingTop: CGFloat = 8
     static let segmentContainerViewHeight: CGFloat = 45
     static let segmentedControllerHeight: CGFloat = 40
     static let bottomUnderlineViewBottomPadding: CGFloat = 4
     static let bottomUnderlineViewHeight: CGFloat = 2
+    static let balanceViewHeight: CGFloat = 100
+    static let horizontalPadding: CGFloat = 16
+    static let balanceTitlePaddingTop: CGFloat = 20
+    static let balanceLabelPaddingTop: CGFloat = 8
+    static let calendarRadius: CGFloat = 23
 }
 
 final class HomeViewController: UIViewController {
@@ -20,6 +26,25 @@ final class HomeViewController: UIViewController {
 // MARK: - Properties
     
     private let output: HomeOutputProtocol
+    
+    private lazy var calendarView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = Constants.calendarRadius
+        view.backgroundColor = .calendarBackground
+        return view
+    }()
+    private lazy var calendarIV: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "calendar")
+        return iv
+    }()
+    private lazy var calendarLabel: UILabel = {
+        let label = UILabel()
+        label.text = "с 01.07.2023 по 01.08.2023"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .white
+        return label
+    }()
     
     private lazy var segmentContainerView: UIView = {
         let view = UIView()
@@ -44,6 +69,26 @@ final class HomeViewController: UIViewController {
         view.backgroundColor = .bottomUnderline
         return view
     }()
+    
+    private lazy var balanceView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .balanceBanckground
+        return view
+    }()
+    private lazy var balanceTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Текущий остаток"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .titleColorGray
+        return label
+    }()
+    private lazy var balanceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "110 000,89 c"
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.textColor = .incomeCash
+        return label
+    }()
 
 // MARK: - Lifecycle
     
@@ -66,11 +111,30 @@ final class HomeViewController: UIViewController {
 // MARK: - Helpers
     
     private func configureUI() {
-        view.addSubview(segmentContainerView)
+        navigationController?.navigationBar.addSubview(calendarView)
+        calendarView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-Constants.horizontalPadding)
+            make.bottom.equalToSuperview()
+        }
         
+        calendarView.addSubview(calendarIV)
+        calendarIV.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.centerY.equalToSuperview()
+        }
+        
+        calendarView.addSubview(calendarLabel)
+        calendarLabel.snp.makeConstraints { make in
+            make.leading.equalTo(calendarIV.snp.trailing).offset(Constants.horizontalPadding)
+            make.centerY.equalToSuperview()
+        }
+        
+        view.addSubview(segmentContainerView)
         segmentContainerView.snp.makeConstraints { make in
             make.leading.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(Constants.segmentContainerPaddingTop)
             make.trailing.equalToSuperview()
             make.height.equalTo(Constants.segmentContainerViewHeight)
         }
@@ -89,6 +153,25 @@ final class HomeViewController: UIViewController {
             make.bottom.equalToSuperview().offset(Constants.bottomUnderlineViewBottomPadding)
             make.height.equalTo(Constants.bottomUnderlineViewHeight)
             make.width.equalToSuperview().multipliedBy(1 / CGFloat(segmentedController.numberOfSegments))
+        }
+        
+        view.addSubview(balanceView)
+        balanceView.snp.makeConstraints { make in
+            make.top.equalTo(segmentContainerView.snp.bottom)
+            make.width.equalToSuperview()
+            make.height.equalTo(Constants.balanceViewHeight)
+        }
+        
+        balanceView.addSubview(balanceTitleLabel)
+        balanceTitleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.top.equalToSuperview().offset(Constants.balanceTitlePaddingTop)
+        }
+        
+        balanceView.addSubview(balanceLabel)
+        balanceLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.top.equalTo(balanceTitleLabel.snp.bottom).offset(Constants.balanceLabelPaddingTop)
         }
         
         view.backgroundColor = .background
