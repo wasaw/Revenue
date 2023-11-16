e33a754dffef934f255f8a8404dd7be8f6d85165
wasaw <merenkovalexander@gmail.com>
2023-11-16 12:17:16 +0300

    Create segmentController for home screen

--- Revenue/Presentation/View/HomeViewController.swift
+++ Revenue/Presentation/View/HomeViewController.swift
@@ -6,6 +6,14 @@
 //
 
 import UIKit
+import SnapKit
+
+private enum Constants {
+    static let segmentContainerViewHeight: CGFloat = 45
+    static let segmentedControllerHeight: CGFloat = 40
+    static let bottomUnderlineViewBottomPadding: CGFloat = 4
+    static let bottomUnderlineViewHeight: CGFloat = 2
+}
 
 final class HomeViewController: UIViewController {
     
@@ -13,6 +21,30 @@ final class HomeViewController: UIViewController {
     
     private let output: HomeOutputProtocol
     
+    private lazy var segmentContainerView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private lazy var segmentItems = ["Остаток", "Доходы", "Расходы", "Цели"]
+    private lazy var segmentedController: UISegmentedControl = {
+        let sc = UISegmentedControl(items: segmentItems)
+        sc.selectedSegmentIndex = 0
+        sc.setTitleTextAttributes([.foregroundColor: UIColor.lightGray,
+                                   .font: UIFont.systemFont(ofSize: 16)], for: .normal)
+        sc.setTitleTextAttributes([.foregroundColor: UIColor.white,
+                                   .font: UIFont.systemFont(ofSize: 16)], for: .selected)
+        sc.selectedSegmentTintColor = .clear
+        sc.tintColor = .clear
+        sc.backgroundColor = .clear
+        sc.addTarget(self, action: #selector(segmentedControlValueChange), for: .valueChanged)
+        return sc
+    }()
+    private lazy var bottomUnderlineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .bottomUnderline
+        return view
+    }()
+
 // MARK: - Lifecycle
     
     init(output: HomeOutputProtocol) {
@@ -28,7 +60,52 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureUI()
+    }
+    
+// MARK: - Helpers
+    
+    private func configureUI() {
+        view.addSubview(segmentContainerView)
+        
+        segmentContainerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.trailing.equalToSuperview()
+            make.height.equalTo(Constants.segmentContainerViewHeight)
+        }
+        
+        segmentContainerView.addSubview(segmentedController)
+        segmentedController.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(Constants.segmentedControllerHeight)
+            make.centerY.equalToSuperview()
+        }
+        
+        segmentContainerView.addSubview(bottomUnderlineView)
+        bottomUnderlineView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview().offset(Constants.bottomUnderlineViewBottomPadding)
+            make.height.equalTo(Constants.bottomUnderlineViewHeight)
+            make.width.equalToSuperview().multipliedBy(1 / CGFloat(segmentedController.numberOfSegments))
+        }
+        
         view.backgroundColor = .background
+    }
+    
+// MARK: - Selecter
+    
+    @objc private func segmentedControlValueChange(_ sender: UISegmentedControl) {
+        let segmentIndex = CGFloat(segmentedController.selectedSegmentIndex)
+        let segmentWidth = segmentedController.frame.width / CGFloat(segmentedController.numberOfSegments)
+        let leadingDistance = segmentWidth * segmentIndex
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            self?.bottomUnderlineView.snp.updateConstraints({ make in
+                make.leading.equalToSuperview().offset(leadingDistance)
+            })
+            self?.view.layoutIfNeeded()
+        }
     }
 }
 
--- Revenue/Utils/Extensions.swift
+++ Revenue/Utils/Extensions.swift
@@ -11,4 +11,5 @@ import UIKit
 
 extension UIColor {
     static let background = UIColor(displayP3Red: 53/255, green: 49/255, blue: 59/255, alpha: 1)
+    static let bottomUnderline = UIColor(displayP3Red: 129/255, green: 244/255, blue: 74/255, alpha: 1)
 }
