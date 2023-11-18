72c1c3ced3e86288f329f8fdd2c1bb5cb91998e4
wasaw <merenkovalexander@gmail.com>
2023-11-18 20:58:19 +0300

    Create UI detailTransactionView

--- Revenue/Presentation/DetailedTransaction/View/DetailedTransactionViewController.swift
+++ Revenue/Presentation/DetailedTransaction/View/DetailedTransactionViewController.swift
@@ -6,6 +6,15 @@
 //
 
 import UIKit
+import SnapKit
+
+private enum Constants {
+    static let detailPaddingTop: CGFloat = 10
+    static let detailViewHeight: CGFloat = 224
+    static let horizontalPadding: CGFloat = 16
+    static let verticalPadding: CGFloat = 16
+    static let dividerHeight: CGFloat = 0.8
+}
 
 final class DetailedTransactionViewController: UIViewController {
     
@@ -24,6 +33,63 @@ final class DetailedTransactionViewController: UIViewController {
         btn.setImage(UIImage(named: "chevron-left"), for: .normal)
         btn.addTarget(self, action: #selector(handleBackButton), for: .touchUpInside)
         return btn
+    }()
+    private lazy var detailTransactionView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    private lazy var typeView: UIView = {
+        let view = UIView()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTypeView))
+        view.addGestureRecognizer(tap)
+        return view
+    }()
+    private lazy var typeImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "salary")
+        return iv
+    }()
+    private lazy var typeTitleLabel: UITextField = {
+        let label = UITextField()
+        label.text = "Заработная плата"
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    private lazy var choiceTypeImage: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "chevron-down")
+        return iv
+    }()
+    private lazy var dividerTopView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .backgroundLightGray
+        return view
+    }()
+    private lazy var dividerBottomView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .backgroundLightGray
+        return view
+    }()
+    
+    private lazy var commentTextFiled: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Комментарий"
+        return tf
+    }()
+    
+    private lazy var amountTitleLabel: UITextField = {
+        let label = UITextField()
+        label.text = "Сумма"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .titleColorGray
+        return label
+    }()
+    private lazy var amoutTextField: UITextField = {
+        let tf = UITextField()
+        tf.text = "30000c"
+        tf.font = UIFont.systemFont(ofSize: 16)
+        return tf
     }()
     
 // MARK: - Lifecycle
@@ -59,6 +125,13 @@ final class DetailedTransactionViewController: UIViewController {
 // MARK: - Helpers
     
     private func configureUI() {
+        configureNavigationItem()
+        configureDetailView()
+        configureComment()
+        configureAmountView()
+    }
+    
+    private func configureNavigationItem() {
         navigationItem.title = "Детали"
 
         let rightBarButton = UIBarButtonItem(customView: deleteButton)
@@ -68,6 +141,83 @@ final class DetailedTransactionViewController: UIViewController {
         navigationItem.leftBarButtonItem = leftBarButton
     }
     
+    private func configureDetailView() {
+        view.addSubview(detailTransactionView)
+        detailTransactionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(Constants.detailPaddingTop)
+            make.width.equalToSuperview()
+            make.height.equalTo(Constants.detailViewHeight)
+        }
+        
+        detailTransactionView.addSubview(typeView)
+        typeView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(Constants.detailViewHeight / 3)
+        }
+        
+        detailTransactionView.addSubview(typeImageView)
+        typeImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.top.equalToSuperview().offset(Constants.verticalPadding)
+        }
+        
+        detailTransactionView.addSubview(typeTitleLabel)
+        typeTitleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(typeImageView.snp.trailing).offset(Constants.horizontalPadding)
+            make.centerY.equalTo(typeImageView.snp.centerY)
+        }
+        
+        detailTransactionView.addSubview(choiceTypeImage)
+        choiceTypeImage.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-Constants.horizontalPadding)
+            make.centerY.equalTo(typeImageView.snp.centerY)
+        }
+    }
+    
+    private func configureComment() {
+        detailTransactionView.addSubview(dividerTopView)
+        dividerTopView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.top.equalTo(typeImageView.snp.bottom).offset(Constants.verticalPadding)
+            make.trailing.equalToSuperview().offset(-Constants.horizontalPadding)
+            make.height.equalTo(Constants.dividerHeight)
+        }
+        
+        detailTransactionView.addSubview(commentTextFiled)
+        commentTextFiled.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.top.equalTo(dividerTopView.snp.bottom)
+            make.trailing.equalToSuperview().offset(-Constants.horizontalPadding)
+            make.height.equalTo(Constants.detailViewHeight / 3)
+        }
+        
+        detailTransactionView.addSubview(dividerBottomView)
+        dividerBottomView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.top.equalTo(commentTextFiled.snp.bottom)
+            make.trailing.equalToSuperview().offset(-Constants.horizontalPadding)
+            make.height.equalTo(Constants.dividerHeight)
+        }
+    }
+    
+    private func configureAmountView() {
+        detailTransactionView.addSubview(amountTitleLabel)
+        amountTitleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.top.equalTo(dividerBottomView.snp.bottom).offset(Constants.verticalPadding)
+            make.trailing.equalToSuperview().offset(-Constants.horizontalPadding)
+        }
+        
+        detailTransactionView.addSubview(amoutTextField)
+        amoutTextField.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(Constants.horizontalPadding)
+            make.trailing.equalToSuperview().offset(-Constants.horizontalPadding)
+            make.bottom.equalToSuperview().offset(-Constants.verticalPadding)
+        }
+    }
+        
 // MARK: - Selectors
     
     @objc private func handleDeleteButton() {
@@ -75,6 +225,9 @@ final class DetailedTransactionViewController: UIViewController {
     
     @objc private func handleBackButton() {
         navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @objc private func handleTypeView() {
     }
 }
 
